9c6ccd19832af21a7be929d0d10de43ce210b5a0
C0B23118 <c0b2311885@edu.teu.ac.jp>
2024-06-11 16:04:29 +0900

    練習4　こうかとんの描画

--- pygame_image.py
+++ pygame_image.py
@@ -17,6 +17,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         screen.blit(bg_img, [0, 0])
+        img_rct=kk_img.get_rect()
+        img_rct.center=300,200
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
